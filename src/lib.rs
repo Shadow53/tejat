ed043da570391430eaec6b5798c898be35d5f341
Michael Bryant <shadow53@shadow53.com>
2022-05-28 20:41:13 -0700

    feat: add Mime support

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 //pub mod gemtext;
-pub mod mime;
+pub use mime;
 pub mod status;
 pub mod uri;
+
+pub fn mime_is_gemtext(m: &mime::Mime) -> bool {
+    m.essence_str().eq_ignore_ascii_case("text/gemini")
+}
+
+pub fn mime_charset(m: &mime::Mime) -> mime::Name {
+    m.get_param(mime::CHARSET).unwrap_or(mime::UTF_8)
+}
 
 #[cfg(test)]
 mod tests {
